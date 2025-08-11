5df95223f688901529ce136d7ac08a1ac7f9bd6a
adilkhoumsiservice-spec <adilkhoumsiservice@gmail.com>
2025-08-11 21:05:27 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,18 @@
-// pages/_document.js
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Meta Tag الخاص بـ AdSense */}
-        <meta
-          name="google-adsense-account"
-          content="ca-pub-8551068222525793"
-        />
-
-        {/* كود Auto Ads من AdSense */}
+        {/* كود التحقق الخاص بـ Google AdSense */}
+        <meta name="google-adsense-account" content="ca-pub-8551068222525793" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8551068222525793"
           crossOrigin="anonymous"
         ></script>
       </Head>
-      <body style={{ backgroundColor: "#ffffff", color: "#000" }}>
+      <body>
         <Main />
         <NextScript />
       </body>
